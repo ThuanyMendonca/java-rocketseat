8df18b69c9725239ee943f53198c77c1ae09de17
Thuany Mendonca <thuany1997@hotmail.com>
2023-10-15 12:28:37 -0300

    Feat(finish) Adding project finished

--- src/main/java/com/github/javarocketseat/todolist/user/UserController.java
+++ src/main/java/com/github/javarocketseat/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package com.github.javarocketseat.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null) {
+            System.out.println("usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuário já existe");
+        }
+
+        String passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
+
+        UserModel userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
