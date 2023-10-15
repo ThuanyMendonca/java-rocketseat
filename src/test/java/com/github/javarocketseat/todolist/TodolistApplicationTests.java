357cf2967960b120a961a9b77acbb6dd83d6373e
Thuany Mendonca <thuany1997@hotmail.com>
2023-10-15 08:07:04 -0300

    Feat(initial) First class users

---
+++ src/test/java/com/github/javarocketseat/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javarocketseat.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
